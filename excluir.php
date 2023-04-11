6f29880f392bfd25477a7453fa20324af3597498
Bruno Barratella <104685289+BrunoBarratella@users.noreply.github.com>
2023-04-11 10:14:38 -0300

    Update excluir.php

--- excluir.php
+++ excluir.php
@@ -1,16 +1,23 @@
 <?php
 
+//Faço o requerimento da classe de conexão e instancio em uma variavel
 require_once "./classes/conexao_class.php";
 $conexao = new Conexao();
 
+//Minha variavel id recebe o valor vindo via metodo $_GET 
 $id = $_GET['deleta'];
 
+//Crio a condição onde se houver um $id ele vai executar os comando abaixo
 if($id){
+  //Acesso minha conexão PDO e preparo a query(Comando SQL)
   $qr = $conexao->conn->prepare("DELETE FROM teste WHERE id = :id");
+  //Seto o valor do :id, como o valor da variavel $id
   $qr->bindValue(':id', $id);
+  //Executo a Query que foi preparada
   $qr->execute();
 }
 
+//Após toda a função se tudo tiver sido executado corretamente vai me mandar novamente para a pagina de listagem de cadastros
 header("Location: listaUsuarios.php");
 
 ?>
